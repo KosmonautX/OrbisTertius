1d438d711ebd8122fb55babfc138c51995df629b
junwei <limjunwei567@gmail.com>
2021-03-06 03:36:14 +0800

    hotfix

--- HeimdallrNode/route_paths/orb_net.js
+++ HeimdallrNode/route_paths/orb_net.js
@@ -1,7 +1,7 @@
 module.exports = (app, verifyToken) => {
 	app.use(`/api/orb`, verifyToken, require(`../routes/orb_net`));
 	// app.use(`/api/orb`, require(`../routes/orb_net`));
-	app.use(`/api/query`, verifyToken, require(`../routes/orb_query`));
+	app.use(`/api/query`, require(`../routes/orb_query`));
 	app.use(`/api/dev`, require(`../routes/orb_dev`));
 	app.use(`/api/comment`, require(`../routes/comment`));
 	app.use(`/api/tele`, require(`../routes/tele`));
