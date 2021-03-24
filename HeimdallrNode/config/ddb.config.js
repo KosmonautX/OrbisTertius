ded5480904ebe266b5b65cff4d3d3a391f4ca89e
junwei <limjunwei567@gmail.com>
2021-03-24 22:45:58 +0800

    Update ddb.config.js

--- HeimdallrNode/config/ddb.config.js
+++ HeimdallrNode/config/ddb.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    dyna: process.env.DYNA,
+    dyna: process.env.DYNA || "http://localhost:8000",
     sthree: process.env.STHREE || "http://localstack:3000",
     mercury: "http://mercury:3000",
-    sthreebucket: process.env.BUCKET || "orbistertius" ,
-    region: "ap-southeast-1",
+    sthreebucket: "orbis-tertius" ,
+    region: "localhost",
+    AWSAccessKeyId: "scratchbac",
+    AWSSecretKey: "scratchbac1",
     tableNames: {
-        orb_table: process.env.TABLE || "ORB_NET"
+        orb_table: "ORB_NET"
     },
 };
