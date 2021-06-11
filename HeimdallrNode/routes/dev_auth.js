159ccf8b240f2a1c78ec7bbe2cef1dbb9edf4af0
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2021-06-11 22:11:32 +0800

    NodeMailer + Templates

--- HeimdallrNode/routes/dev_auth.js
+++ HeimdallrNode/routes/dev_auth.js
@@ -5,27 +5,17 @@ const land = require('../controller/graphLand').Land
 const crypto = require('crypto')
 const jwt = require(`jsonwebtoken`);
 const buffer = require('buffer');
-const axios = require('axios')
+const moment = require('moment');
+let mail = require('../controller/mailAngareion').Angareion.Mail
 const algorithm = 'aes-256-ctr';
 const ENCRYPTION_KEY = process.env.SECRET_TUNNEL;
 const IV_LENGTH = 16;
 
-async function sms(body, messagebody) {
-    let url = "http://10.12.184.21:8082/"
-    let config = {
-      headers:{
-        Authorization:'3f4e4360'
-      }
-    }
-      return await axios.post(url, {
-        to: body.number,
-        message: messagebody
-        }, config);
-}
-
 router.get('/tele', async (req, res, next) =>
     {
       try{
+        // moment.unix check with auth_date parameter +5mins
+        if(req.query.auth_date> moment().subtract(600, 'seconds').unix()){
         const bot_token = process.env.NEIB
         const secretKey = crypto.createHash('sha256')
                                 .update(bot_token)
@@ -70,17 +60,70 @@ router.get('/tele', async (req, res, next) =>
           err.status = 401
           throw err;
         }
-
+        }
+        else if (req.query.auth_date <= moment().subtract(600, 'seconds').unix()) {
+          let err = new Error("Expired Authentication")
+          err.status = 401
+          throw err;
+        }
       }
       catch (err) {
         next(err);
     }}
 )
 
+router.get('/mailin', async (req, res ,next ) =>
+  {
+    try {
+		const iss = "ppmail";
+		const sub = "sb";
+		const exp = "10min";
+		const verifyOptions = {
+			issuer : iss,
+			subject : sub,
+			maxAge : exp,
+			algorithms : ["HS256"]
+		};
+      if (req.query.hash) {
+			  req.verification = jwt.verify(req.query.hash, secret, verifyOptions);
+        req.query.id = decrypt(req.verification).split('#')[0]
+        var user = land.Entity;
+        user.claim("USR", req.query.id,"pte");
+        payload= await user.upsert().catch(err => {
+            err.status = 400;
+            next(err);
+          });
+          if(payload.Attributes){
+          res.status(201).json({
+            "Returning User": req.query.id,
+            "Home Postal": payload.Attributes.numeric,
+            "Office Postal": payload.Attributes.geohash,
+            "Last Login": payload.Attributes.time
+          })
+          }
+          else{
+            res.status(201).json({
+            "Creating User": req.query.id
+          })
+          }
+
+		} else {
+			let err = new Error(`No token, please login again!`);
+			err.status = 401;
+			next(err);
+		}
+    } catch (err) {
+		if (err.message == "maxAge exceeded") err.status = 403;
+        if (err.message == "jwt expired") err.status = 403;
+      	next(err);
+    }
+
+  }
+          );
+
 router.get('/handshake',(req, res ,next ) =>
   {
     try{
-
 
 
     }catch(err){
@@ -90,7 +133,6 @@ router.get('/handshake',(req, res ,next ) =>
 
   }
           );
-
 router.post('/server' , async (req,res, next) => {
   // integrate handshake (AES128 ECDH-ES)
   // device id check and integration for exists
@@ -135,40 +177,45 @@ router.post('/server' , async (req,res, next) => {
 
 
 
-router.post('/message', async (req, res, next) => {
+router.post('/mail', async (req, res, next) => {
   var encrypted;
+  var signup = true
   let message = {}
-  user_id = uuidv4();
-  let payload = land.Entity.affirm("TEL", req.body.code+ req.body.number,user_id )
+  let user_id = uuidv4();
+  let nonce = '#' + crypto.randomBytes(16).toString('base64')
+  // device id addition to login path and check on uuid existence
+  let payload = land.Entity.affirm("MSG", req.body.mail,user_id )
   payload.then(response => {
   if(payload.Attributes){
     user_id = payload.Attributes.alphanumeric
+    signup = false
   }})
   .catch(error => {
     console.log(error);
   })
-  message.hash = encrypt(req.body.number)
-  const secretKey = crypto.createHash('sha256')
-                          .update(process.env.SECRET_TUNNEL)
-                                .digest();
-  const hmac = crypto.createHmac('sha256', secretKey)
-                     .update(message.hash)
-              .digest('hex');
-  let redirect = "https://i.scratchbac.org/?hash="+hmac+"&id="+message.hash
-  let reply =sms(req.body, redirect)
-    reply.then(response => {
+  message.hash = encrypt(user_id + nonce)
+  const iss = 'ppmail';
+        const sub = 'sb';
+        const exp = '10min'
+        const signOptions = {
+            issuer: iss,
+            subject: sub,
+            expiresIn: exp,
+            algorithm: 'HS256',
+        };
+  const token = jwt.sign(message, ENCRYPTION_KEY, signOptions);
+  let redirect = "https://i.scratchbac.org/?hash="+ token +"&id=" + signup
+  let sent  = mail.signup(req.body, redirect)
+    sent.then(response => {
       res.status(201).json({
-        "Await": "User"
+        "Await": redirect
       })})
   .catch(error => {
     console.log(error);
-    error = new Error("SMS Gateway Failed");
+    error = new Error("Email Failed");
       error.status = 500;
       next(error);
   });
-
-
-      
            
 } );
 
