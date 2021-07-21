3beb24b633e3a7fce2d0068c744fb459ad61047d
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2021-07-21 12:17:27 +0800

    Media Handling User

--- HeimdallrNode/routes/orb_query.js
+++ HeimdallrNode/routes/orb_query.js
@@ -63,6 +63,7 @@ router.get(`/get_user`, async function (req, res, next) {
         if (pubData.Item.payload) {
             dao.bio = pubData.Item.payload.bio;
             dao.profile_pic = pubData.Item.payload.profile_pic;
+            dao.media = pubData.Item.payload.media
             if(pubData.Item.payload.media) dao.media_asset = await serve3.preSign('getObject','USR',req.query.user_id,'1920x1080')
             //dao.verified = pubData.Item.payload.verified;
             // dao.country_code = pteData.Item.payload.country_code;
@@ -388,7 +389,7 @@ router.get(`/orbs_in_loc_fresh_batch`, async function (req, res, next) {
                         if(item.payload.media) dao.payload.media_asset = await serve3.preSign('getObject','ORB',dao.orb_uuid,'150x150')
                         if(item.payload.init){
                             dao.payload.init = item.payload.init;
-                            if(item.payload.init.media) dao.payload.init.media_asset = await serve3.preSign('getObject','ORB',item.payload.user_id,'150x150')}
+                            if(item.payload.init.media) dao.payload.init.media_asset = await serve3.preSign('getObject','USR',item.payload.user_id,'150x150')}
                     }
                     page.push(dao);
                 }
