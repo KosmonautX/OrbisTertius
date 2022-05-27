fe3527dc8238d9851b293ee694dffae4a1f196d0
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2022-05-27 20:01:21 +0800

    User Channel Geosubscriptions Groundworks

--- PhosphorusPhoenix/lib/phos_web/channels/user_channel.ex
+++ PhosphorusPhoenix/lib/phos_web/channels/user_channel.ex
@@ -3,25 +3,41 @@ defmodule PhosWeb.UserChannel do
   alias PhosWeb.Menshen.Auth
   alias Phos.Message
   alias Phos.Action
+  alias Phos.Pubsub
   @impl true
   def join("archetype:usr:" <> id , _payload, socket) do
     if authorized?(socket, id) do
       send(self(), :initiation)
-      {:ok, socket |> assign(:user_channel_id, id)}
+      {:ok, socket
+      |> assign(:user_channel_id, id)
+      |> assign(:geolocation, %{})}
+      |> assign(:geosubscriptions, [])
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # maintain subscriber lists
-  def handle_info(:geoinitiation,  socket) do
-    # watch out for backpressure
+  # maintain subscriber lists for live home work on socket
+  def handle_in("geocenter", geolocation,  socket) do
+    # watch out for backpressure genstage here Producer/Consumer
     ref = socket_ref(socket)
     Task.start(fn ->
+      #subscription inside tasks
       Phoenix.Channel.reply(ref, {:ok, Action.list_orbs()})
       end)
-    {:noreply,socket}
+
+    {:noreply, socket |> geosubscribing(geolocation)}
   end
+
+  defp geosubscribing(socket, geolocation) do
+    geolocation
+    |> Enum.map(fn {k,v} ->
+      unless socket[k]["hash"] == v["hash"] do
+        #replace with geosubscriptions
+      end
+    end)
+  end
+end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
@@ -43,7 +59,7 @@ defmodule PhosWeb.UserChannel do
       {:ok, struct} ->
         echo = Map.take(struct, [:destination, :source, :source_archetype, :destination_archetype, :message, :inserted_at, :subject, :subject_archetype])
         |> Map.update!(:inserted_at, &(&1 |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix() |> to_string()))
-        broadcast socket, "shout", echo #broadcast to both channels from and to, first the source
+        broadcast socket, "shout", echo #broadcast to both channels from and to, first the source as shout event
         PhosWeb.Endpoint.broadcast_from!(self(), "archetype:usr:" <> echo.destination, "shout", echo) #then  broadcast to destination as well
         #fyring and forgetting
         Phos.Fyr.Task.start_link(Pigeon.FCM.Notification.new({:topic, "USR." <> echo.destination}, %{"title" => "Message from #{socket.assigns.user_agent["username"]}", "body" => echo.message},echo))
@@ -67,9 +83,9 @@ defmodule PhosWeb.UserChannel do
                                            time: DateTime.from_naive!(echoes.inserted_at,"Etc/UTC") |> DateTime.to_unix()
                                    }) end)
     {:noreply,socket}
-  end
+   end
 
-  # Add authorization logic here as required.
+  # Add authorization logic here as required. Process send_after for auth channel
   defp authorized?(socket, id) do
     case Auth.validate(socket.assigns.session_token) do
       {:ok , claims} ->
