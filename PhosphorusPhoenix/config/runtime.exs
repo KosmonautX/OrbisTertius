a6f16775d4034d7ed93cf89d8b82a460eb98acfa
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2022-06-09 17:52:22 +0800

    AWS SECRET ENV UPDATE

--- PhosphorusPhoenix/config/runtime.exs
+++ PhosphorusPhoenix/config/runtime.exs
@@ -31,7 +31,7 @@ unless config_env() == :prod do
   # AWS
   config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
-  secret_access_key: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+  secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
   region: "ap-southeast-1"
 
 end
