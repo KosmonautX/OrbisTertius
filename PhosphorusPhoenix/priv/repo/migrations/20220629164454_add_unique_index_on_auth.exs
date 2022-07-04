5674b0070d80f46307d786eb3bb003b3e1078a94
Satrio Nugroho <satrio.nugroho@sinarmasmining.com>
2022-07-04 09:29:56 +0700

    add disable ddl transaction

--- PhosphorusPhoenix/priv/repo/migrations/20220629164454_add_unique_index_on_auth.exs
+++ PhosphorusPhoenix/priv/repo/migrations/20220629164454_add_unique_index_on_auth.exs
@@ -1,5 +1,6 @@
 defmodule Phos.Repo.Migrations.AddUniqueIndexOnAuth do
   use Ecto.Migration
+  @disable_ddl_transaction true
 
   def change do
     create unique_index(:user_auths, [:auth_id, :auth_provider, :user_id], concurrently: true)
