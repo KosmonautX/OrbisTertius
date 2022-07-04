cb92932f547abc95331faa2d53c5c1ccbd6d2a56
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2022-07-04 11:36:54 +0800

    Disabling Migration Lock Concurrent Index Disabling DDL transactions removes the guarantee that all of the changes in the migration will happen at once. Disabling the migration lock removes the guarantee only a single node will run a given migration if multiple nodes are attempting to migrate at the same time.

--- PhosphorusPhoenix/priv/repo/migrations/20220629164454_add_unique_index_on_auth.exs
+++ PhosphorusPhoenix/priv/repo/migrations/20220629164454_add_unique_index_on_auth.exs
@@ -1,6 +1,7 @@
 defmodule Phos.Repo.Migrations.AddUniqueIndexOnAuth do
   use Ecto.Migration
   @disable_ddl_transaction true
+  @disable_migration_lock true
 
   def change do
     create unique_index(:user_auths, [:auth_id, :auth_provider, :user_id], concurrently: true)
