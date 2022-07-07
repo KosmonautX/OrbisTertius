b17c1ac607cb4aa2363cf1e111b3fa7c51bcbe0b
KosmonautX <avivekbala@gmail.com>
2022-07-07 06:58:41 +0000

    Swoosh Mailer API Client Update

--- PhosphorusPhoenix/config/config.exs
+++ PhosphorusPhoenix/config/config.exs
@@ -28,7 +28,7 @@ config :phos, PhosWeb.Endpoint,
 config :phos, Phos.Mailer, adapter: Swoosh.Adapters.Local
 
 # Swoosh API client is needed for adapters other than SMTP.
-config :swoosh, :api_client, false
+# config :swoosh, :api_client, false
 
 # Configure esbuild (the version is required)
 config :esbuild,
