c5dbc806bd0d6a2fd93e86210f6a3d8f40b92fb7
Satrio Nugroho <satrio.nugroho@sinarmasmining.com>
2022-07-20 19:06:13 +0700

    notion WIP import

---
+++ PhosphorusPhoenix/lib/phos/external/sector.ex
@@ -0,0 +1,17 @@
+defmodule Phos.External.Sector do
+  def get do
+    case HTTPoison.get(sector_url()) do
+      {:ok, %HTTPoison.Response{body: body, status_code: 200}} -> Phoenix.json_library().decode!(body)
+      {:error, err} -> HTTPoison.Error.message(err)
+    end
+  end
+
+  defp sector_url() do
+    case Keyword.get(config(), :url) do
+      url when is_binary(url) -> url
+      _ -> raise RuntimeError, "Sector URL not found."
+    end
+  end
+
+  defp config(), do: Application.get_env(:phos, __MODULE__, [])
+end
---
+++ PhosphorusPhoenix/lib/phos_web/live/admin/orb_live/import.ex
@@ -0,0 +1,7 @@
+defmodule PhosWeb.Admin.OrbLive.Import do
+  use PhosWeb, :admin_view
+
+  def mount(params, _session, socket) do
+    {:ok, socket}
+  end
+end
