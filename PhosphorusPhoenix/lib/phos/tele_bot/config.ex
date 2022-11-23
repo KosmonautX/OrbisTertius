1f173b4b6d037f532bbf978557baafe97921ce35
Satrio Nugroho <satrio@hey.com>
2022-11-23 04:48:01 +0000

    ensure to use https on callback url
    Signed-off-by: Satrio Nugroho <satrio@hey.com>

--- PhosphorusPhoenix/lib/phos/tele_bot/config.ex
+++ PhosphorusPhoenix/lib/phos/tele_bot/config.ex
@@ -8,10 +8,19 @@ defmodule Phos.TeleBot.Config do
       {:ok, config} -> Keyword.get(config, key, default) |> eval()
       _ -> default
     end
+    |> ensure_https(key)
   end
 
   defp eval({mod, func, args}) when is_atom(mod) and is_atom(func), do: apply(mod, func, args)
   defp eval(data), do: data
+
+  defp ensure_https(data, :callback_url) do
+    data
+    |> URI.parse()
+    |> Map.put(:scheme, "https")
+    |> URI.to_string()
+  end
+  defp ensure_https(data, _key), do: data
 
   defp config() do
     Application.get_env(:phos, Phos.TeleBot, [])
