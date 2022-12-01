e867b12896c0d5cc236b1c30d0d3305df8be8741
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2022-12-02 03:19:31 +0800

    User Tests Cleanup

--- PhosphorusPhoenix/lib/phos/users.ex
+++ PhosphorusPhoenix/lib/phos/users.ex
@@ -488,7 +488,7 @@ defmodule Phos.Users do
   {:ok, %{to: ..., body: ...}}
 
   """
-  def deliver_update_email_instructions(%User{} = user, current_email, update_email_url_fun)
+  def deliver_user_update_email_instructions(%User{} = user, current_email, update_email_url_fun)
   when is_function(update_email_url_fun, 1) do
     {encoded_token, user_token} = UserToken.build_email_token(user, "change:#{current_email}")
 
@@ -560,7 +560,7 @@ defmodule Phos.Users do
   @doc """
   Deletes the signed token with the given context.
   """
-  def delete_session_token(token) do
+  def delete_user_session_token(token) do
     Repo.delete_all(UserToken.token_and_context_query(token, "session"))
     :ok
   end
--- PhosphorusPhoenix/test/phos_web/live/user_forgot_password_live_test.exs
+++ PhosphorusPhoenix/test/phos_web/live/user_forgot_password_live_test.exs
@@ -12,7 +12,7 @@ defmodule PhosWeb.UserForgotPasswordLiveTest do
       {:ok, _lv, html} = live(conn, ~p"/users/reset_password")
 
       assert html =~ "Forgot your password?"
-      assert html =~ "Register</a>"
+      assert html =~ "Sign up</a>"
       assert html =~ "Log in</a>"
     end
 
