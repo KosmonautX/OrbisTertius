c8b4c310f21ab7795ec62302744ba748af22969a
Satrio Nugroho <satrio@hey.com>
2022-12-13 07:05:19 +0000

    add util viewer test
    Signed-off-by: Satrio Nugroho <satrio@hey.com>

--- PhosphorusPhoenix/test/phos_web/util/viewer_test.exs
+++ PhosphorusPhoenix/test/phos_web/util/viewer_test.exs
@@ -1,24 +1,27 @@
 defmodule PhosWeb.Util.ViewerTest do
   use Phos.DataCase
 
-  alias Phoenix.View
   alias PhosWeb.Util.Viewer
 
-  describe "orb_mapper/1" do
-    orb_1 = orb_fixture(%{title: "First orb"})
-    orb_2 = orb_fixture(%{title: "Second orb"})
+  import Phos.ActionFixtures, only: [orb_fixture: 1]
 
+  describe "orb_mapper/1" do
     test "when given data is single orb" do
-      keys = ~w(expiry_time active orb_uuid title relationship creationtime mutationtime source payload geolocation parent media)a
-      assert data = Viewer.orb_mapper(orb_1)
+      orb = orb_fixture(%{"title" => "First orb"})
+      keys = ~w(expiry_time active orb_uuid title relationships creationtime mutationtime source payload geolocation parent media traits)a
+      assert data = Viewer.orb_mapper(orb)
       assert Enum.sort(Map.keys(data)) == Enum.sort(keys)
     end
 
     test "when given data is list" do
+      keys = ~w(expiry_time active orb_uuid title relationships creationtime mutationtime source payload geolocation parent media traits)a
+      orb_1 = orb_fixture(%{"title" => "First orb"})
+      orb_2 = orb_fixture(%{"title" => "Second orb"})
       data = [orb_1, orb_2]
 
       assert data = Viewer.orb_mapper(data)
       assert Kernel.length(data) == 2
+      assert Enum.sort(Map.keys(List.first(data))) == Enum.sort(keys)
     end
   end
   
