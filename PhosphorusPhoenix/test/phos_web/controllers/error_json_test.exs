2c7ea464c6ba35e9f6d28f228a25b58d0d56ab68
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2022-12-27 13:08:37 +0800

    Test Cases Patch for Error Mssage Handling

--- PhosphorusPhoenix/test/phos_web/controllers/error_json_test.exs
+++ PhosphorusPhoenix/test/phos_web/controllers/error_json_test.exs
@@ -2,11 +2,11 @@ defmodule PhosWeb.ErrorJSONTest do
   use PhosWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PhosWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PhosWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{message: "Not Found"}}
   end
 
   test "renders 500" do
     assert PhosWeb.ErrorJSON.render("500.json", %{}) ==
-             %{errors: %{detail: "Internal Server Error"}}
+             %{errors: %{message: "Internal Server Error"}}
   end
 end
