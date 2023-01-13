00ad2122a46b28017b41db6d39552d8191dafcad
κϩⲐⲡⲓז‎ς_ⲙ0ᚱⲣⲉ𐤅ᛊ <johndoe@example.com>
2023-01-14 01:07:07 +0800

    Test Cases Restored Chat Notifs Functional

--- PhosphorusPhoenix/test/phos_web/live/user_login_live_test.exs
+++ PhosphorusPhoenix/test/phos_web/live/user_login_live_test.exs
@@ -51,9 +51,9 @@ defmodule PhosWeb.UserLoginLiveTest do
 
       conn = submit_form(form, conn)
 
-      #assert Phoenix.Flash.get(conn.assigns.flash, :error) == "Invalid email or password"
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) == "Invalid email or password"
 
-      #assert redirected_to(conn) == "/users/log_in"
+      assert redirected_to(conn) == "/users/log_in"
     end
   end
 
