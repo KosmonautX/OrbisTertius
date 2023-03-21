27dd45470831c829c89221c6125abb12f0d8b2cf
ks0m1c_dharma <johndoe@dharma.in>
2023-03-22 05:17:42 +0800

    Break Words Fix for Information Card

--- PhosphorusPhoenix/assets/js/app.js
+++ PhosphorusPhoenix/assets/js/app.js
@@ -34,7 +34,7 @@ let liveSocket = new LiveSocket("/live", Socket, {
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
+topbar.config({ barColors: { 0: "#00A86B" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.delayedShow(200))
 window.addEventListener("phx:page-loading-stop", () => {
   topbar.hide()
