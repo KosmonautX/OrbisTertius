c7a906f00e4b5c2638745faebf46f996d0808644
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-05-31 12:08:18 +0800

    Missed out virtual field in previous commit

--- PhosphorusPhoenix/lib/phos/users/user.ex
+++ PhosphorusPhoenix/lib/phos/users/user.ex
@@ -24,6 +24,8 @@ defmodule Phos.Users.User do
     field :self_relation, :string, virtual: true
     field :profile_image, :string, virtual: true
 
+    field :count, :integer, default: 0, virtual: true
+
     # has_many :pending_relations, RelationBranch, foreign_key: :user_id, where: [completed_at: nil]
     # has_many :completed_relations, RelationBranch, foreign_key: :user_id, where: [completed_at: {:not, nil}]
     # #has_many :friends, through: [:completed_relations, :friend]
