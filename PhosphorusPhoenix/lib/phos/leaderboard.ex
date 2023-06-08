1e6d299fda908b244b85839365d591b4a65645c6
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-06-08 12:21:52 +0800

    Leaderboard orb streams working, cleaned code up

--- PhosphorusPhoenix/lib/phos/leaderboard.ex
+++ PhosphorusPhoenix/lib/phos/leaderboard.ex
@@ -11,9 +11,9 @@ defmodule Phos.Leaderboard do
   #   User
   #   |> Repo.Paginated.all(limit: limit, page: page)
   # end
-  def list_user_counts(limit, page, :orbs, filters) do
-    startdt = Keyword.get(filters, :startdt)
-    enddt = Keyword.get(filters, :enddt)
+  def list_user_counts(limit, page, :orbs, filter_dates) do
+    startdt = Keyword.get(filter_dates, :startdt)
+    enddt = Keyword.get(filter_dates, :enddt)
 
     from(u in User,
     join: o in assoc(u, :orbs),
@@ -25,19 +25,25 @@ defmodule Phos.Leaderboard do
     |> Repo.Paginated.all(limit: limit, page: page)
   end
 
-  def list_user_counts(limit, page, :chats) do
+  def list_user_counts(limit, page, :chats, filter_dates) do
+    startdt = Keyword.get(filter_dates, :startdt)
+    enddt = Keyword.get(filter_dates, :enddt)
+
     query =
       from(m in Memory,
-        join: u in User,
-        on: m.user_source_id == u.id,
-        group_by: [u.id, m.rel_subject_id],
-        order_by: [desc: count(m)],
-        select: %{uid: u.id, r_id: m.rel_subject_id}
-      )
+      join: u in User,
+      on: m.user_source_id == u.id,
+      group_by: [u.id, m.rel_subject_id, m.inserted_at],
+      order_by: [desc: count(m)],
+      distinct: [u.id, m.rel_subject_id],
+      select: %{uid: u.id, r_id: m.rel_subject_id, inserted_at: m.inserted_at}
+    )
 
     from(u in User,
       join: q in subquery(query),
       on: u.id == q.uid,
+      where: q.inserted_at > ^startdt,
+      where: q.inserted_at < ^enddt,
       group_by: u.id,
       order_by: [desc: count(u.id)],
       select_merge: %{count: count(u.id)}
--- PhosphorusPhoenix/lib/phos_web/live/admin/leaderboard_live/index.ex
+++ PhosphorusPhoenix/lib/phos_web/live/admin/leaderboard_live/index.ex
@@ -1,86 +1,86 @@
 defmodule PhosWeb.Admin.LeaderboardLive.Index do
   use PhosWeb, :admin_view
-
-  alias Phos.Repo
   alias Phos.Leaderboard
-  alias Phos.Action.Orb
-
 
   def mount(_params, _session, socket) do
-    limit = 20
-    page = 1
-    startdt = DateTime.utc_now() |> DateTime.add(-365, :day)
-    enddt = DateTime.utc_now()
-
-    %{data: users, meta: user_meta} = Leaderboard.list_user_counts(limit, page, :orbs, [startdt: startdt, enddt: enddt])
-    %{data: orbs} = Leaderboard.rank_orbs(limit, page)
-
-
-    # %{data: orbs, meta: orb_meta} = Leaderboard.rank_orbs(limit, page)
-
-
-
-    filter_dates = %{startdate: startdt |> DateTime.to_date(), enddate: enddt |> DateTime.to_date()}
-
    {:ok,
     socket
-    |> assign(orb_view: false)
-    |> assign(orbs: orbs)
-    |> assign(:filter_by, "orbs")
-    |> assign(limit: limit)
-    |> assign(:users, users)
-    |> stream(:users, users)
-    |> assign(:user_meta, user_meta)
-    |> assign(:filter_dates, filter_dates)
-  }
+    |> setup_assign()
+    }
   end
-
-  def handle_params(_params, _url, socket), do: {:noreply, socket}
 
   def handle_event(
-      "load-more",
-      _,
-      %{assigns: %{filter_by: option, filter_dates: %{startdate: startdate, enddate: enddate}, limit: limit, user_meta: %{pagination: pagination}}} = socket
-      ) do
-      IO.inspect(option)
-      expected_page = pagination.current + 1
+    "load-more-users",
+    _,
+    %{assigns: %{filter_by: option, filter_dates: %{} = filter_dates, limit: limit, user_meta: %{pagination: pagination}}} = socket
+    ) do
+    expected_page = pagination.current + 1
 
-      with {:ok, startdt} <- NaiveDateTime.from_iso8601("#{startdate} 00:00:00"),
-           {:ok, enddt} <- NaiveDateTime.from_iso8601("#{enddate} 23:59:59"),
-           true <- NaiveDateTime.diff(startdt, enddt) < 0
-      do
-        %{data: newusers, meta: newmeta} = Leaderboard.list_user_counts(limit, expected_page, String.to_atom(option), [startdt: startdt, enddt: enddt])
-        newsocket = Enum.reduce(newusers, socket, fn user, acc -> stream_insert(acc, :users, user) end)
-        {:noreply, newsocket |> assign(user_meta: newmeta)}
-      else
-        _ -> {:noreply, socket}
-      end
+    with {:ok, %{startdt: startdt, enddt: enddt}} <- get_naive_dates(filter_dates) do
+      %{data: new_users, meta: new_meta} = Leaderboard.list_user_counts(limit, expected_page, String.to_atom(option), [startdt: startdt, enddt: enddt])
+      newsocket = Enum.reduce(new_users, socket, fn user, acc -> stream_insert(acc, :users, user) end)
+
+      {:noreply,
+      newsocket
+      |> assign(user_meta: new_meta)}
+    else
+      _ -> {:noreply, socket}
+    end
+
+  end
+
+  def handle_event(
+    "load-more-orbs",
+    _,
+    %{assigns: %{limit: limit, orb_meta: %{pagination: pagination}}} = socket
+    ) do
+    expected_page = pagination.current + 1
+
+    %{data: new_orbs, meta: new_meta} = Leaderboard.rank_orbs(limit, expected_page)
+    newsocket = Enum.reduce(new_orbs, socket, fn orb, acc -> stream_insert(acc, :orbs, orb) end)
+
+    {:noreply,
+    newsocket
+    |> assign(orb_meta: new_meta)}
 
   end
 
-  def handle_event("filter",
-                  %{"filter_by" => option, "user" => %{"startdate" => startdate, "enddate" => enddate}},
-                  %{assigns: %{limit: limit, user_meta: %{pagination: pagination} = user_meta}} = socket
-                  ) do
+  def handle_event(
+    "filter",
+    %{"filter_by" => option, "user" => %{"startdate" => startdate, "enddate" => enddate}},
+    %{assigns: %{limit: limit}} = socket
+    ) do
 
-      with {:ok, startdt} <- NaiveDateTime.from_iso8601("#{startdate} 00:00:00"),
-           {:ok, enddt} <- NaiveDateTime.from_iso8601("#{enddate} 23:59:59"),
-            true <- NaiveDateTime.diff(startdt, enddt) < 0
-      do
-        %{data: users} = Leaderboard.list_user_counts(limit, 1, String.to_atom(option), [startdt: startdt, enddt: enddt])
-        {:noreply,
-        socket
-        |> stream(:users, users, reset: true)
-        |> assign(user_meta: user_meta)
-        |> assign(orb_view: false)
-        |> assign(filter_by: option)
-        }
+    filter_dates = %{startdate: startdate, enddate: enddate}
 
-      else
-        _ -> {:noreply, socket}
-      end
+    with {:ok, %{startdt: startdt, enddt: enddt}} <- get_naive_dates(filter_dates) do
+      %{data: new_users, meta: new_meta} = Leaderboard.list_user_counts(limit, 1, String.to_atom(option), [startdt: startdt, enddt: enddt])
+
+      {:noreply,
+      socket
+      |> stream(:users, new_users, reset: true)
+      |> assign(user_meta: new_meta)
+      |> assign(orb_view: false)
+      |> assign(filter_by: option)
+      |> assign(filter_dates: filter_dates)
+      }
+
+    else
+      _ -> {:noreply, socket}
+    end
 
   end
+
+  def handle_event("reset", _, socket) do
+    IO.inspect(socket.assigns.streams)
+
+    {:noreply,
+     socket
+     |> stream(:users, [], reset: true)
+     |> stream(:orbs, [], reset: true)
+     |> setup_assign()
+    }
+   end
 
   def handle_event("orb_rank", _, socket) do
     {:noreply,
@@ -89,27 +89,47 @@ defmodule PhosWeb.Admin.LeaderboardLive.Index do
     }
   end
 
-  defp stream_assign(socket, key, %{data: data, meta: meta} = params) do
+  defp setup_assign(socket) do
+    limit = 40
+    page = 1
+    startdt = DateTime.utc_now() |> DateTime.add(-60, :day)
+    enddt = DateTime.utc_now()
+
+    %{data: users, meta: user_meta} = Leaderboard.list_user_counts(limit, page, :orbs, [startdt: startdt, enddt: enddt])
+    %{data: orbs, meta: orb_meta} = Leaderboard.rank_orbs(limit, page)
+
+    filter_dates = %{startdate: startdt |> DateTime.to_date(), enddate: enddt |> DateTime.to_date()}
+
     socket
-    |> stream(key, data)
-    |> assign(key, meta)
+    |> assign(orb_view: false)
+    # |> assign(orbs: orbs)
+    |> assign(orb_meta: orb_meta)
+
+    |> assign(:filter_by, "orbs")
+    |> assign(:filter_dates, filter_dates)
+    |> assign(limit: limit)
+
+    # |> assign(:users, users)
+    |> assign(:user_meta, user_meta)
+
+    |> stream(:users, users)
+    |> stream(:orbs, orbs)
+
   end
 
-  # def multi_filter_orbs(filters, opts \\ []) do
-  #   page = Keyword.get(opts, :page, 1)
-  #   limit = Keyword.get(opts, :limit, 20)
-  #   sort_attribute = Keyword.get(opts, :sort_attribute, :inserted_at)
-  #   startdt = Keyword.get(filters, :startdt)
-  #   enddt = Keyword.get(filters, :enddt)
+  defp get_naive_dates(%{startdate: startdate, enddate: enddate} = filter_dates) do
 
-  #   query =
-  #     [
-  #       &where(&1, [o], o.inserted_at > ^startdt),
-  #       &where(&1, [o], o.inserted_at < ^enddt)
-  #     ]
-  #     |> Enum.reduce(Phos.Action.Orb, fn x, acc -> acc |> x.() end)
-  #     |> preload(:initiator)
+    with {:ok, startdt} <- NaiveDateTime.from_iso8601("#{startdate} 00:00:00"),
+      {:ok, enddt} <- NaiveDateTime.from_iso8601("#{enddate} 23:59:59"),
+      true <- NaiveDateTime.diff(startdt, enddt) < 0
+    do
+      {:ok, %{startdt: startdt, enddt: enddt}}
 
-  #     Repo.Paginated.all(query, page, sort_attribute, limit)
-  # end
+    else
+      _ ->
+        {:error, filter_dates}
+
+    end
+  end
+
 end
