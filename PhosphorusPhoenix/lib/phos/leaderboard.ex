3692404bce0b055860c93c009b4629aa0eeda8a0
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2023-06-14 20:31:37 +0800

    Broke down between two dates into a fn

--- PhosphorusPhoenix/lib/phos/leaderboard.ex
+++ PhosphorusPhoenix/lib/phos/leaderboard.ex
@@ -9,22 +9,17 @@ defmodule Phos.Leaderboard do
 
 
   def list_user_counts(limit, page, :orbs, filter_dates) do
-    startdt = Keyword.get(filter_dates, :startdt)
-    enddt = Keyword.get(filter_dates, :enddt)
 
     from(u in User,
     join: o in assoc(u, :orbs),
-    where: o.inserted_at > ^startdt,
-    where: o.inserted_at < ^enddt,
     group_by: u.id,
     order_by: [desc: count(o)],
     select_merge: %{count: count(o)})
+    |> between_dates(filter_dates)
     |> Repo.Paginated.all(limit: limit, page: page, aggregate: false)
   end
 
   def list_user_counts(limit, page, :chats, filter_dates) do
-    startdt = Keyword.get(filter_dates, :startdt)
-    enddt = Keyword.get(filter_dates, :enddt)
 
     query =
       from(m in Memory,
@@ -39,61 +34,51 @@ defmodule Phos.Leaderboard do
     from(u in User,
       join: q in subquery(query),
       on: u.id == q.uid,
-      where: q.inserted_at > ^startdt,
-      where: q.inserted_at < ^enddt,
       group_by: u.id,
       order_by: [desc: count(u.id)],
       select_merge: %{count: count(u.id)}
     )
+    |> between_dates(filter_dates)
     |> Repo.Paginated.all(limit: limit, page: page, aggregate: false)
   end
 
   def list_user_counts(limit, page, :comments, filter_dates) do
-    startdt = Keyword.get(filter_dates, :startdt)
-    enddt = Keyword.get(filter_dates, :enddt)
 
     from(u in User,
     join: c in Comment,
     on: c.initiator_id == u.id,
-    where: c.inserted_at > ^startdt,
-    where: c.inserted_at < ^enddt,
     group_by: u.id,
     order_by: [desc: count(c)],
     select_merge: %{count: count(c)})
+    |> between_dates(filter_dates)
     |> Repo.Paginated.all(limit: limit, page: page, aggregate: false)
   end
 
 
 
   def list_user_counts(limit, page, :allies, filter_dates) do
-    startdt = Keyword.get(filter_dates, :startdt)
-    enddt = Keyword.get(filter_dates, :enddt)
 
     from(u in User,
     join: r in assoc(u, :relations),
-    where: r.completed_at > ^startdt,
-    where: r.completed_at < ^enddt,
     group_by: u.id,
     order_by: [desc: count(r)],
     select_merge: %{count: count(r)},
     limit: 20)
+    |> between_dates(filter_dates)
     |> Repo.Paginated.all(limit: limit, page: page, aggregate: false)
   end
 
 
   def rank_orbs(limit, page, filter_dates) do
-    startdt = Keyword.get(filter_dates, :startdt)
-    enddt = Keyword.get(filter_dates, :enddt)
 
     from(o in Orb,
     preload: :initiator,
     join: c in assoc(o, :comments),
-    where: c.inserted_at > ^startdt,
-    where: c.inserted_at < ^enddt,
     group_by: o.id,
     order_by: [desc: count(c)],
     select_merge: %{comment_count: count(c)}
     )
+    |> between_dates(filter_dates)
     |> Repo.Paginated.all(limit: limit, page: page, aggregate: false)
   end
 
@@ -103,5 +88,14 @@ defmodule Phos.Leaderboard do
     end
   end
 
+  defp between_dates(query, filter_dates) do
+    startdt = Keyword.get(filter_dates, :startdt)
+    enddt = Keyword.get(filter_dates, :enddt)
+
+    query
+    |> where([q], q.inserted_at > ^startdt)
+    |> where([q], q.inserted_at < ^enddt)
+
+  end
 
 end
