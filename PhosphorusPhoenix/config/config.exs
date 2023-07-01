97a322edb0652f63773ff446190e21321d84af2c
Satrio Nugroho <satrio.nu27@gmail.com>
2023-07-02 01:11:42 +0700

    introduce NER

--- PhosphorusPhoenix/config/config.exs
+++ PhosphorusPhoenix/config/config.exs
@@ -134,4 +134,11 @@ config :phos, Phos.PlatformNotification,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
+
+config :nx, default_backend: EXLA.Backend
+
+config :phos, Phos.Models.TokenClassification,
+  model: "dslim/bert-base-NER",
+  token: "bert-base-cased"
+
 import_config "#{config_env()}.exs"
