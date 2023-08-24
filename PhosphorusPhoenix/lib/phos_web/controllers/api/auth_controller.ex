36dc238b4a694bf9f9815e4e3e8138d5cb8b75b8
KosmonautX <avivekbala@gmail.com>
2023-08-24 19:02:28 +0800

    Bump Version to 2.1.0

--- PhosphorusPhoenix/lib/phos_web/controllers/api/auth_controller.ex
+++ PhosphorusPhoenix/lib/phos_web/controllers/api/auth_controller.ex
@@ -51,8 +51,8 @@ defmodule PhosWeb.API.FyrAuthController do
   end
 
   def semver(conn, %{"version" => version}) do
-    latest = "1.6.1" # latest patch
-    earliest = "1.6.0" # below current minor version
+    latest = "2.1.1" # latest patch
+    earliest = "2.0.0" # below current minor version
     response = cond do
       Version.match?(version, ">= " <> latest) ->
         "ignore"
